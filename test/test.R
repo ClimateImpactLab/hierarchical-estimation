e2c9d0233834601a1cd1078860c65a6964283bbf
James Rising <jarising@gmail.com>
2017-05-10 18:04:27 -0700

    changed test suite

--- test/test.R
+++ test/test.R
@@ -1,12 +1,12 @@
 library(RUnit)
 
-testsuite.interpolate <- defineTestSuite("interpolate",
-                                         dirs = file.path("test/interpolate"),
-                                         testFileRegexp = "^test_.+\\.R",
-                                         testFuncRegexp = "^test.+",
-                                         rngKind = "Marsaglia-Multicarry",
-                                         rngNormalKind = "Kinderman-Ramage")
+testsuite.logspec <- defineTestSuite("logspec",
+                                     dirs = file.path("test/logspec"),
+                                     testFileRegexp = "^test_.+\\.R",
+                                     testFuncRegexp = "^test.+",
+                                     rngKind = "Marsaglia-Multicarry",
+                                     rngNormalKind = "Kinderman-Ramage")
 
-testResult <- runTestSuite(testsuite.interpolate)
+testResult <- runTestSuite(testsuite.logspec)
 
 printTextProtocol(testResult, showDetails = TRUE)
